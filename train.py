b37ff9f588b9244b433cae66aec0f6abc3c927c1
EUOD <2024201303@aust.edu.cn>
2026-02-12 16:05:30 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,9 +3,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('/root/RTDETR-main/ultralytics/cfg/models/rt-detr/rtdetr-p2.yaml')
-    # model.load('') # loading pretrain weights
-    model.train(data='/root/RTDETR-main/dataset/UDOD/UDOD.yaml',
+    model = RTDETR('/root/RTDETR-main/ultralytics/cfg/models/rt-detr/rtdetr.yaml')
+    model.train(data='/root/RTDETR-main/dataset/UDD/UDD.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=200,
